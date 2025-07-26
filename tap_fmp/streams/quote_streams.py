0e4f04f492fc10811a2e194939831d613fffdada
melgazar9 <melgazar9@gmail.com>
2025-07-26 10:14:53 -0500

    placeholder streams for bulk, earnings transcript, and quotes

---
+++ tap_fmp/streams/quote_streams.py
@@ -0,0 +1,53 @@
+class StockQuoteStream:
+    name = "stock_quote"
+
+class StockQuoteShortStream:
+    name = "stock_quote_short"
+
+class AftermarketTradeStream:
+    name = "aftermarket_trade"
+
+class AftermarketQuoteStream:
+    name = "aftermarket_quote"
+
+class StockPriceChangeStream:
+    name = "stock_price_change"
+
+class StockBatchStream:
+    name = "stock_batch"
+
+class StockBatchQuoteStream:
+    name = "stock_batch_quote"
+
+class StockBatchQuoteShortStream:
+    name = "stock_batch_quote_short"
+
+class BatchAftermarketTradeStream:
+    name = "batch_aftermarket_trade"
+
+class BatchAftermarketQuoteStream:
+    name = "batch_aftermarket_quote"
+
+class BatchListStream:
+    name = "batch_list"
+
+class ExchangeStockQuotesStream:
+    name = "exchange_stock_quotes"
+
+class MutualFundPriceQuotesStream:
+    name = "mutual_fund_price_quotes"
+
+class ETFPriceQuotesStream:
+    name = "etf_price_quotes"
+
+class FullCommoditiesQuotesStream:
+    name = "full_commodities_quotes"
+
+class FullCryptocurrencyQuotesStream:
+    name = "full_cryptocurrency_quotes"
+
+class FullForexQuoteStream:
+    name = "full_forex_quote"
+
+class FullIndexQuotesStream:
+    name = "full_index_quotes"
